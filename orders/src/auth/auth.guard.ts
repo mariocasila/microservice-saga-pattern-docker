76b089ad95a3e3749c9cdc6ec76b03e234b77edc
Mario Casila <mario.casila.94@gmail.com>
2024-02-06 02:20:10 -0800

    added auth guard

---
+++ orders/src/auth/auth.guard.ts
@@ -0,0 +1,50 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { JWTConfig } from 'src/configs/jwt.config';
+import { CustomCatch } from 'src/common/exception/custom-catch';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly jwtStrategy: JwtStrategy,
+    private readonly jwtConfig: JWTConfig,
+  ) {}
+
+  @CustomCatch()
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    if (!request.headers.authorization) {
+      return false;
+    }
+
+    if (
+      request.headers.authorization &&
+      (request.headers.authorization as string).split(' ')[0] === 'Bearer'
+    ) {
+      const token = (request.headers.authorization as string).split(' ')[1];
+      const decodedToken: any = this.jwtService.decode(token);
+
+      // if (
+      //   !decodedToken ||
+      //   !decodedToken.exp ||
+      //   decodedToken.exp * 1000 < Date.now()
+      // ) {
+      //   throw new UnauthorizedException('Token expired');
+      // }
+      console.log('request.headers.authorization=', decodedToken)
+
+      request.user = await this.jwtStrategy.validate(decodedToken);
+      return true;
+    }
+
+    throw new UnauthorizedException('Unauthorized');
+  }
+}
